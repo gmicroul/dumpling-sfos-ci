63a5de61c547c7d17ded2b9cc23049f00d828c3e
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-11-20 02:25:02 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -3,13 +3,13 @@ export PLATFORM_SDK_ROOT="/srv/mer"
 export ANDROID_ROOT="/parentroot/srv/hadk"
 export VENDOR="oneplus"
 export DEVICE="dumpling"
-export PORT_ARCH="armv7hl"
-export RELEASE="4.6.0.13"
+export PORT_ARCH="aarch64"
+export RELEASE="5.0.0.21"
 
 #sudo mkdir -p /proc/sys/fs/binfmt_misc/
 #sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sudo mic create fs --arch=armv7hl \
-                   --tokenmap=ARCH:armv7hl,RELEASE:$RELEASE,EXTRA_NAME:"$EXTRA_NAME" \
+                   --tokenmap=ARCH:$PORT_ARCH,RELEASE:$RELEASE,EXTRA_NAME:"$EXTRA_NAME" \
                    --record-pkgs=name,url \
                    --outdir=sfe-$DEVICE-$RELEASE"$EXTRA_NAME" \
                    --pack-to=sfe-$DEVICE-$RELEASE"$EXTRA_NAME".tar.bz2 \
