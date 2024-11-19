6df4cdde6e5ac7c413b71937154ae8bfc660f960
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-11-19 21:46:31 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo mic create fs --arch=$PORT_ARCH \
-                   --tokenmap=ARCH:$PORT_ARCH,RELEASE:$RELEASE,EXTRA_NAME:"$EXTRA_NAME" \
+sudo mic create fs --arch=armv7hl \
+                   --tokenmap=ARCH:armv7hl,RELEASE:$RELEASE,EXTRA_NAME:"$EXTRA_NAME" \
                    --record-pkgs=name,url \
                    --outdir=sfe-$DEVICE-$RELEASE"$EXTRA_NAME" \
                    --pack-to=sfe-$DEVICE-$RELEASE"$EXTRA_NAME".tar.bz2 \
