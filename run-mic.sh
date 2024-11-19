0c6cade3e0746148a2215e62e43cb2a6fa6aa492
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-11-19 22:21:11 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -5,6 +5,8 @@ export VENDOR="oneplus"
 export DEVICE="dumpling"
 export PORT_ARCH="armv7hl"
 export RELEASE="3.4.0.24"
+
+sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sudo mic create fs --arch=armv7hl \
                    --tokenmap=ARCH:armv7hl,RELEASE:$RELEASE,EXTRA_NAME:"$EXTRA_NAME" \
                    --record-pkgs=name,url \
