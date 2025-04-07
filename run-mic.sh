1e4693f8e0eed0d8d48b1a5b904503554c130cb8
gmicroul <121873438+gmicroul@users.noreply.github.com>
2025-04-07 15:40:19 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -4,8 +4,8 @@ export ANDROID_ROOT="/parentroot/srv/hadk"
 export VENDOR="oneplus"
 export DEVICE="dumpling"
 export PORT_ARCH="armv7hl"
-#export RELEASE="3.4.0.24"
-export RELEASE="5.0.0.62"
+export RELEASE="3.4.0.24"
+#export RELEASE="5.0.0.62"
 
 sudo mkdir -p /proc/sys/fs/binfmt_misc/
 sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
